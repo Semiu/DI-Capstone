9e1f8ed1b1066c2e5332675dd82383af309c602b
Semiu <ayobami.sm@gmail.com>
2019-09-23 00:54:14 -0500

    After large file removal

--- webapp/index.js
+++ webapp/index.js
@@ -1,19 +1,1 @@
 //
-$("#findMyWeather").click(function(event) {
-  event.preventDefault();
-  $(".alert").hide();
-  if ($("#city").val()!="") {
-    $.get("scraper.php?city="+$("#city").val(),
-    function( data ) {
-      if (data=="") {
-        $("#fail").fadeIn();
-      }else {
-        $("#success").html(data).fadeIn();
-      }
-    });
-  }else{
-    $("#noCity").fadeIn();
-  }
-});
-//Setting the container div to window's height
-$(.container).css("height", $(window).height());
